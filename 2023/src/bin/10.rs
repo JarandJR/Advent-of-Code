cfa63d793f7a44c3dc67398ff026bfe02aacf151
Jarand Romestrand <jarandjr@stud.ntnu.no>
2023-12-11 18:32:04 +0100

    10_a

--- 2023/src/bin/10.rs
+++ 2023/src/bin/10.rs
@@ -1,82 +1,245 @@
-use aoc2023::{read_file_string, get_data_list, Parse, get_numbers_on_line};
+use std::{fmt::Debug, f32::INFINITY};
+
+use aoc2023::read_file_string;
 
 fn main() {
     let data = read_file_string("inputs/10.txt").unwrap();
     println!("Result 1: {}", solve(data.clone()));
-    println!("Result 2: {}", solve2(data.clone()));
 }
 
-fn solve(data: String) -> i64 {
-    let trees: Vec<Tree> = get_data_list(data);
-    trees.into_iter().map(|t| t.get_next()).fold(0, |a, b| a + b)
-}
+fn solve(data: String) -> i32 {
+    let (mut pipes, start) = parse(&data);
+    let start = start.unwrap();
+    for dir in Direction::directions() {
+        let (x, y) = start.to_tuple();
+        pipes[y][x].visited = true;
+        pipes[y][x].dist = 0;
+        let (ox, oy) = get_other(dir, x, y);
 
-fn solve2(data: String) -> i64 {
-    let trees: Vec<Tree> = get_data_list(data);
-    trees.into_iter().map(|t| t.get_prev()).fold(0, |a, b| a + b)
-}
+        if pipes[oy][ox].ep1.is_none() || pipes[oy][ox].ep2.is_none() {
+            continue;
+        } else if pipes[oy][ox].ep1.unwrap() != Direction::opposite(&dir) && 
+        pipes[oy][ox].ep2.unwrap() != Direction::opposite(&dir) {
+            continue;
+        }
+        let (mut next_x, mut next_y) = pipes[oy][ox].at.to_tuple();
 
-#[derive(Debug)]
-struct Tree {
-    nums: Vec<Vec<i64>>,
-}
+        let mut counter = 1;
+        pipes[next_y][next_x].visited = true;
+        pipes[next_y][next_x].entered = Some(Direction::opposite(&dir));
+        pipes[next_y][next_x].dist = counter;
 
-impl Tree {
-    fn new(n: Vec<i64>) -> Self {
-        let mut nums = vec![n];
-        let mut at = 0;
-        while nums.last().unwrap().iter().sum::<i64>() != 0 {
-            let mut next = Vec::new();
-            for (a, n) in nums[at].iter().enumerate() {
-                if a == nums[at].len() - 1 {
-                    break;
-                }
-                next.push(nums[at][a + 1] - n);
+        while pipes[next_y][next_x].sym != 'S' {
+            counter += 1;
+            let next_dir = pipes[next_y][next_x].get_next_dir();
+            if next_dir.is_none() {
+                break;
             }
-            nums.push(next);
-            at += 1;
+            let next_dir = next_dir.unwrap();
+            let (ox, oy) = get_other(next_dir, next_x, next_y);
+            let res = pipes[next_y][next_x].next(next_dir, &pipes[oy][ox]);
+            if res.is_err() {
+                break;
+            }
+            (next_x, next_y) = res.unwrap();
+
+            if pipes[next_y][next_x].visited {
+                if pipes[next_y][next_x].dist > counter {
+                    pipes[next_y][next_x].dist = counter;
+                }
+            } else {
+                pipes[next_y][next_x].visited = true;
+                pipes[next_y][next_x].dist = counter;
+            }
+            
+            pipes[next_y][next_x].entered = Some(Direction::opposite(&next_dir));
         }
-        Tree { nums, }
+    }
+    pipes
+        .iter()
+        .flat_map(|pipe_vec| pipe_vec.iter())
+        .filter(|pipe| pipe.visited)
+        .map(|pipe| pipe.dist)
+        .max()
+        .unwrap_or(0)
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum Direction {
+    North, 
+    South,
+    West,
+    East,
+}
+
+impl Direction {
+    fn to_value(dir: &Direction) -> (i32, i32) {
+        match dir {
+            Direction::North => (0, -1),
+            Direction::South => (0, 1),
+            Direction::West  => (-1, 0),
+            Direction::East  => (1, 0),
+        }
     }
 
-    fn get_next(&self) -> i64 {
-        self.nums.iter().map(|n| n.last().unwrap()).fold(0, |a, b| a + b)
+    fn directions() -> Vec<Direction> {
+        vec![Direction::North, Direction::South, Direction::West, Direction::East]
     }
 
-    fn get_prev(&self) -> i64 {
-        let mut prev = 0;
-        for n in self.nums.iter().rev() {
-            prev = n.first().unwrap() - prev;
+    fn opposite(dir: &Direction) -> Direction {
+        match dir {
+            Direction::North => Direction::South,
+            Direction::South => Direction::North,
+            Direction::West  => Direction::East,
+            Direction::East  => Direction::West,
         }
-        prev
     }
 }
 
-impl Parse for Tree {
-    fn parse(line: &str) -> Self {
-        Tree::new(get_numbers_on_line(line))
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32, 
+    y: i32,
+}
+
+impl Point {
+    fn to_tuple(&self) -> (usize, usize) {
+        (self.x as usize, self.y as usize)
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, PartialEq)]
+struct Pipe {
+    sym: char,
+    at: Point,
+    ep1: Option<Direction>,
+    ep2: Option<Direction>,
+    visited: bool,
+    entered: Option<Direction>,
+    dist: i32,
+}
 
-    #[test]
-    fn test_10_1a() {
-        assert_eq!(8, solve("..F7.
-    .FJ|.
-    SJ.L7
-    |F--J
-    LJ...".to_string()));
+impl Pipe {
+    fn new(at: Point, sym: char) -> Self {
+        let  dirs = Pipe::get_directions(sym);
+        let mut ep1 = None;
+        let mut ep2 = None;
+        if dirs.is_some() {
+            let (e1, e2) = dirs.unwrap();
+            ep1 = Some(e1);
+            ep2 = Some(e2);
+        }
+        Self { sym, at, ep1, ep2, visited: false , entered: None , dist: INFINITY as i32 }
     }
 
-    #[test]
-    fn test_10_1b() {
-        assert_eq!(4, solve2(".....
-    .S-7.
-    .|.|.
-    .L-J.
+    fn get_directions(sym: char) -> Option<(Direction, Direction)>{
+        match sym {
+            '|' => Some((Direction::North, Direction::South)),
+            '-' => Some((Direction::West, Direction::East)),
+            'L' => Some((Direction::North, Direction::East)),
+            'J' => Some((Direction::West, Direction::North)),
+            '7' => Some((Direction::West, Direction::South)),
+            'F' => Some((Direction::South, Direction::East)),
+            _ => None,
+        }
+    }
+
+    fn next(&self, dir: Direction, other: &Pipe) -> Result<(usize, usize), &str> {
+        if other == self {
+            return Err("Same pipe");
+        }
+        if self.is_valid_move(&dir, other) {
+            return Ok(other.at.to_tuple());
+        }
+        Err("Not a valid move")
+    }
+
+    fn is_valid_move(&self, dir: &Direction, other: &Pipe) -> bool {
+        if self.ep1.is_none() || 
+        self.ep2.is_none() {
+            return false;
+        }
+        if other.ep1.is_none() || 
+        other.ep2.is_none() {
+            if other.sym == 'S' {
+                return true;
+            }
+            return false;
+        }
+        if dir != self.ep1.as_ref().unwrap() && 
+        dir != self.ep2.as_ref().unwrap() {
+            return false;
+        }
+        let (x, y) = Direction::to_value(dir);
+        dir == &Direction::opposite(other.ep1.as_ref().unwrap()) || 
+        dir == &Direction::opposite(other.ep2.as_ref().unwrap()) && 
+        self.at.x + x == other.at.x && 
+        self.at.y + y == other.at.y
+    }
+
+    fn get_next_dir(&self) -> Option<Direction> {
+        let dir = &self.entered.unwrap();
+        if self.ep1.is_none() || 
+        self.ep2.is_none() {
+            return None;
+        }
+        if dir != self.ep1.as_ref().unwrap() && 
+        dir != self.ep2.as_ref().unwrap() {
+            return None;
+        }
+        if dir == self.ep1.as_ref().unwrap() {
+            return self.ep2;
+        }
+        self.ep1
+    }
+}
+
+fn parse(data: &str) -> (Vec<Vec<Pipe>>, Option<Point>) {
+    let mut pipes = Vec::new();
+    let mut start_point = None;
+
+    let mut y = 0;
+    for l in data.lines() {
+        let mut x = 0;
+        let mut row = Vec::new();
+        for c in l.chars().into_iter() {
+            if c == 'S' {
+                start_point = Some(Point { x, y });
+            }
+            row.push(Pipe::new(Point { x, y }, c));
+            x += 1;
+        }
+        pipes.push(row);
+        y += 1;
+    }
+    (pipes, start_point)
+}
+
+fn get_other(dir: Direction, x: usize, y: usize) -> (usize, usize) {
+    let (dx, dy) = Direction::to_value(&dir);
+    let (mut x, mut y) = (x as i32 + dx, y as i32 + dy);
+    if x < 0 {
+        x = 0;
+    } else if y < 0 {
+        y = 0;
+    }
+    (x as usize, y as usize)
+}
+
+#[test]
+fn test_10_1a() {
+    assert_eq!(4, solve(".....
+.S-7.
+.|.|.
+.L-J.
     .....".to_string()));
-    }
+}
+
+#[test]
+fn test_10_1b() {
+    assert_eq!(8, solve("..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...".to_string()));
 }
