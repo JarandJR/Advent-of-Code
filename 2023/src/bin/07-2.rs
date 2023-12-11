dfb5704b13d955a7a82996ee6a1661384890f42e
Jarand Romestrand <jarandjr@stud.ntnu.no>
2023-12-11 02:00:28 +0100

    07

--- 2023/src/bin/07-2.rs
+++ 2023/src/bin/07-2.rs
@@ -118,11 +118,16 @@ fn get_card_number_string(c: char) -> String {
     }
 }
 
-#[test]
-fn test_07_2() {
-    assert_eq!(5905, solve("32T3K 765
-T55J5 684
-KK677 28
-KTJJT 220
-QQQJA 483".to_string()));
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_07_2() {
+        assert_eq!(5905, solve("32T3K 765
+    T55J5 684
+    KK677 28
+    KTJJT 220
+    QQQJA 483".to_string()));
+    }
 }
