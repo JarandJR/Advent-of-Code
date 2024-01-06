e0a0ef85c0d9e0a77c40ff41644562877174806d
Jarand Romestrand <jarandjr@stud.ntnu.no>
2024-01-06 17:52:56 +0100

    12-1

--- 2023/src/bin/12-1.rs
+++ 2023/src/bin/12-1.rs
@@ -1,7 +1,7 @@
 use aoc2023::read_file_string;
 
 fn main() {
-    let data = read_file_string("inputs/11.txt").unwrap();
+    let data = read_file_string("inputs/12.txt").unwrap();
     println!("Result: {}", solve(data));
 }
 
@@ -14,7 +14,7 @@ fn solve(data: String) -> usize {
     .fold(0, |acc, f| acc + f)
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Records {
     Operational,
     Damaged,
@@ -34,10 +34,60 @@ impl From<char> for Records {
 
 fn get_arrangements(line: &str) -> usize {
     let (records, parts) = line.split_once(' ').unwrap();
-    let records = records.chars().map(|c| Records::from(c)).collect::<Vec<Records>>();
+    let mut records = records.chars().map(|c| Records::from(c)).collect::<Vec<Records>>();
     let parts = parts.split(',').map(|s| s.parse::<usize>().unwrap()).collect::<Vec<usize>>();
-    dbg!(records, parts);
-    todo!()
+
+    generate_combinations(&mut records)
+        .iter()
+        .filter(|a| is_valid(a, &parts))
+    .count()
+}
+
+fn generate_combinations(r: &Vec<Records>) -> Vec<Vec<Records>> {
+    let mut combs = Vec::new();
+    
+    let mut stack = Vec::new();
+    stack.push((0, Vec::new()));
+    while let Some((at, mut cur)) = stack.pop() {        
+        for i in at..r.len() {
+            match r[i] {
+                Records::Damaged => cur.push(Records::Damaged),
+                Records::Operational => cur.push(Records::Operational),
+                Records::Unknown => {
+                    let mut next = cur.clone();
+                    cur.push(Records::Operational);
+                    next.push(Records::Damaged);
+                    stack.push((i + 1, next));
+                },
+            }
+        }
+        if cur.len() == r.len() {
+            combs.push(cur);
+            continue;
+        }
+    }
+    combs
+}
+
+fn is_valid(arr: &Vec<Records>, parts: &Vec<usize>) -> bool {
+    let blocks = arr
+        .split(|a| a == &Records::Operational)
+        .filter(|a| a.len() > 0)
+        .collect::<Vec<&[Records]>>();
+    
+    if blocks.len() != parts.len() {
+        return false;
+    }
+
+    let mut i = 0;
+    blocks
+    .iter()
+    .all(|a| {
+        let len = a.len();
+        let part = parts[i];
+        i += 1;
+        len == part
+    })
 }
 
 macro_rules! test_arrangements {
@@ -48,15 +98,12 @@ macro_rules! test_arrangements {
         }
     };
 }
-
-test_arrangements!(test_12_1a_1, "???.### 1,1,3", 1);// 1 "#.?.###" n = 1, k = 1
-test_arrangements!(test_12_1a_2, ".??..??...?##. 1,1,3", 4); // n = 2, k = 1, x2 // n = 4 and k = 1
-test_arrangements!(test_12_1a_3, "?#?#?#?#?#?#?#? 1,3,1,6", 1); // "#######.#?#?#?#?" -> "#######.###.#.#."
-test_arrangements!(test_12_1a_4, "????.#...#... 4,1,1", 1); // n = k = 1
-test_arrangements!(test_12_1a_5, "????.######..#####. 1,6,5", 4);// n = 4, k = 1
-test_arrangements!(test_12_1a_6, "?###???????? 3,2,1", 10); // ".###.#.?????" n = 5, k = 2
-                                                            // ".###.##.????" n=4, k=1 WRONG
-                                                            // Must remove the ones not possible else where and then take the largest
+test_arrangements!(test_12_1a_1, "???.### 1,1,3", 1);
+test_arrangements!(test_12_1a_2, ".??..??...?##. 1,1,3", 4);
+test_arrangements!(test_12_1a_3, "?#?#?#?#?#?#?#? 1,3,1,6", 1);
+test_arrangements!(test_12_1a_4, "????.#...#... 4,1,1", 1);
+test_arrangements!(test_12_1a_5, "????.######..#####. 1,6,5", 4);
+test_arrangements!(test_12_1a_6, "?###???????? 3,2,1", 10);
 
 #[test]
 fn test_12_1b() {
@@ -67,4 +114,3 @@ fn test_12_1b() {
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1".to_string()));
 }
-
