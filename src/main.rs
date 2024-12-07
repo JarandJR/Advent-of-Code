86984bd0030f6d1e07f6f28ee9056a3b187f614d
Jarand Romestrand <jarandjr@stud.ntnu.no>
2024-12-07 21:59:17 +0100

    Added simple script to create file for solving that day

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::fs::File;
+use std::io::Write;
+use chrono::{Datelike, Local};
+
+
 fn main() {
-    println!("hello world");
+    let today = Local::now();
+    let year = today.year().to_string();
+    let day = today.day().to_string();
+    let file_path = format!("{}/src/bin/{}.rs", year, day);
+    {
+        // Setup for test
+        let mut file = File::create(&file_path).expect("Could not create file");
+        let template = include_str!("template.rs");
+        let template = template.replace("DAY", &day);
+        file.write_all(template.as_bytes()).expect("Could not write to file");
+    }
 }
---
+++ src/template.rs
@@ -0,0 +1,33 @@
+use aoc2024::parse_into_lines_automatic;
+
+fn main() {
+    dbg!(parse_and_solve("DAY"));
+}
+
+fn parse_and_solve(day: &str) -> usize {
+    if let Some(line_iter) = parse_into_lines_automatic(day) {
+        return 0;
+    }
+    0
+}
+
+#[test]
+fn dayDAY_1() {
+    use std::fs::{remove_file, File};
+    let file_name = "test_DAY_1";
+    let file_path = format!("inputs/{}.txt", file_name);
+    {
+        // Setup for test
+        use std::io::Write;
+        let mut file = File::create(&file_path).expect("Could not create file");
+        writeln!(
+            file,
+            "INPUT"
+        )
+        .expect("Could not write to file");
+    }
+    let result = parse_and_solve(&file_name);
+    // Clean up
+    remove_file(file_path).expect("Could not remove file");
+    assert_eq!(result, 0);
+}
