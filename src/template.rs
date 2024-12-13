56b54b4b4de1e143c527e3489c6c2888b8c23641
Jarand Romestrand <jarandjr@stud.ntnu.no>
2024-12-13 17:28:30 +0100

    Slite change in template

--- src/template.rs
+++ src/template.rs
@@ -1,10 +1,18 @@
 use common::io::parse_into_lines_automatic;
 
 fn main() {
-    dbg!(parse_and_solve("DAY"));
+    dbg!(parse_and_solve_part_1("DAY"));
+    dbg!(parse_and_solve_part_2("DAY"));
 }
 
-fn parse_and_solve(day: &str) -> usize {
+fn parse_and_solve_part_1(day: &str) -> usize {
+    if let Some(line_iter) = parse_into_lines_automatic(day) {
+        return 0;
+    }
+    0
+}
+
+fn parse_and_solve_part_2(day: &str) -> usize {
     if let Some(line_iter) = parse_into_lines_automatic(day) {
         return 0;
     }
@@ -15,6 +23,27 @@ fn parse_and_solve(day: &str) -> usize {
 fn dayDAY_1() {
     use std::fs::{remove_file, File};
     let file_name = "test_DAY_1";
+    let file_path = format!("inputs/{}.txt", file_name);
+    {
+        // Setup for test
+        use std::io::Write;
+        let mut file = File::create(&file_path).expect("Could not create file");
+        writeln!(
+            file,
+            "INPUT"
+        )
+        .expect("Could not write to file");
+    }
+    let result = parse_and_solve(&file_name);
+    // Clean up
+    remove_file(file_path).expect("Could not remove file");
+    assert_eq!(result, 0);
+}
+
+#[test]
+fn dayDAY_2() {
+    use std::fs::{remove_file, File};
+    let file_name = "test_DAY_2";
     let file_path = format!("inputs/{}.txt", file_name);
     {
         // Setup for test
