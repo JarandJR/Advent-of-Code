5bc8879ceb2b1c11229c984874af6a044df8d95b
Jarand Romestrand <jarandjr@stud.ntnu.no>
2025-12-11 16:57:21 +0100

    Day 10

--- 2025/src/bin/10.rs
+++ 2025/src/bin/10.rs
@@ -1,9 +1,13 @@
 use std::collections::{HashSet, VecDeque};
 
 use common::io::parse_into_lines_automatic;
+use good_lp::{
+    Expression, ProblemVariables, Solution, SolverModel, Variable, default_solver, variable,
+};
 
 fn main() {
     dbg!(parse_and_solve_1(parse_into_lines_automatic("10")));
+    dbg!(parse_and_solve_2(parse_into_lines_automatic("10")));
 }
 
 fn parse_and_solve_1(line_iter: impl Iterator<Item = String>) -> usize {
@@ -14,6 +18,68 @@ fn parse_and_solve_1(line_iter: impl Iterator<Item = String>) -> usize {
             .map(|b| b.iter().fold(0, |acc, n| acc | 1 << n))
             .collect::<Vec<u16>>();
         acc + bfs(fasit, buttons)
+    })
+}
+
+fn parse_and_solve_2(line_iter: impl Iterator<Item = String>) -> usize {
+    line_iter.fold(0, |acc, line| {
+        let (_, buttons, joltage) = parse(line);
+        acc + lp(buttons, joltage)
+    })
+}
+
+fn lp(constraints: Vec<Vec<usize>>, requirements: Vec<usize>) -> usize {
+    let mut vars = ProblemVariables::new();
+    // Variables representing clicks on corresponding button
+    let x = (0..constraints.len())
+        .map(|_| vars.add(variable().integer().min(0)))
+        .collect::<Vec<Variable>>();
+    // Minimize button clicks
+    let objective = x.iter().sum::<Expression>();
+    let mut problem = vars.minimise(objective.clone()).using(default_solver);
+    for (req_pos, req) in requirements.iter().enumerate() {
+        // Button clicks constrained by joltage
+        let constraint = constraints
+            .iter()
+            .enumerate()
+            .filter(|(_, c)| c.contains(&req_pos))
+            .map(|(var_pos, _)| x[var_pos])
+            .sum::<Expression>()
+            .eq(*req as u32);
+        problem = problem.with(constraint);
+    }
+    let solution = problem.solve().unwrap();
+    solution.eval(objective).round() as usize
+}
+
+#[allow(dead_code)]
+// The first naive solution
+fn solve_2_naive_bfs(line_iter: impl Iterator<Item = String>) -> usize {
+    line_iter.fold(0, |acc, line| {
+        let (_, buttons, joltage) = parse(line);
+        let mut visited = HashSet::new();
+        let mut que = VecDeque::new();
+        (0..buttons.len()).for_each(|i| {
+            let mut state = vec![0; joltage.len()];
+            buttons[i].iter().for_each(|b| state[*b] += 1);
+            visited.insert(state.clone());
+            que.push_back((state, 1));
+        });
+        while let Some((state, depth)) = que.pop_front() {
+            if state == joltage {
+                return depth + acc;
+            }
+            for b in &buttons {
+                let mut state = state.clone();
+                b.iter().for_each(|b| state[*b] += 1);
+                if visited.insert(state.clone())
+                    && state.iter().zip(joltage.iter()).all(|(a, b)| a <= b)
+                {
+                    que.push_back((state, depth + 1));
+                }
+            }
+        }
+        panic!("No solution")
     })
 }
 
@@ -71,4 +137,13 @@ fn day10_1() {
 [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}";
     let result = parse_and_solve_1(input.lines().map(|s| s.to_owned()));
     assert_eq!(result, 7);
+}
+
+#[test]
+fn day10_2() {
+    let input = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}";
+    let result = parse_and_solve_2(input.lines().map(|s| s.to_owned()));
+    assert_eq!(result, 33);
 }
