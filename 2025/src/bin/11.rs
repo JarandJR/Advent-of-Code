6db16486dde131c478d3f7c493f18e63a0edb4cb
Jarand Romestrand <jarandjr@stud.ntnu.no>
2025-12-11 14:09:16 +0100

    Day 11

--- 2025/src/bin/11.rs
+++ 2025/src/bin/11.rs
@@ -3,10 +3,11 @@ use std::collections::HashMap;
 use common::{generic_algorithms::dfs::dfs, io::parse_into_lines_automatic};
 
 fn main() {
-    dbg!(parse_and_solve(parse_into_lines_automatic("11")));
+    dbg!(parse_and_solve_1(parse_into_lines_automatic("11")));
+    dbg!(parse_and_solve_2(parse_into_lines_automatic("11")));
 }
 
-fn parse_and_solve(line_iter: impl Iterator<Item = String>) -> usize {
+fn parse_and_solve_1(line_iter: impl Iterator<Item = String>) -> usize {
     let paths = line_iter
         .map(|line| {
             let key = line.chars().take_while(|c| !c.eq(&':')).collect::<String>();
@@ -20,10 +21,50 @@ fn parse_and_solve(line_iter: impl Iterator<Item = String>) -> usize {
         .collect::<HashMap<String, Vec<String>>>();
     let mut memo = HashMap::new();
     dfs(
-        String::from("you"),
+        "you".to_string(),
         &mut memo,
-        &|state| state.eq(&String::from("out")).then(|| 1),
+        &|state| state.eq("out").then(|| 1),
         &|state| paths.get(state).unwrap().to_vec(),
+        &|a, b| a + b,
+    )
+}
+
+fn parse_and_solve_2(line_iter: impl Iterator<Item = String>) -> usize {
+    let paths = line_iter
+        .map(|line| {
+            let key = line.chars().take_while(|c| !c.eq(&':')).collect::<String>();
+            let paths = line
+                .split_whitespace()
+                .skip(1)
+                .map(|s| s.to_owned())
+                .collect::<Vec<String>>();
+            (key, paths)
+        })
+        .collect::<HashMap<String, Vec<String>>>();
+    let mut memo = HashMap::new();
+    dfs(
+        ("svr".to_string(), false, false),
+        &mut memo,
+        &|(state, fft, dac)| {
+            state
+                .as_str()
+                .eq("out")
+                .then(|| (*fft && *dac).then(|| 1).unwrap_or_else(|| 0))
+        },
+        &|(state, fft, dac)| {
+            paths
+                .get(&state.to_string())
+                .unwrap()
+                .iter()
+                .map(|n| {
+                    let mut next_fft = *fft;
+                    let mut next_dac = *dac;
+                    n.eq("fft").then(|| next_fft = true);
+                    n.eq("dac").then(|| next_dac = true);
+                    (n.clone(), next_fft, next_dac)
+                })
+                .collect()
+        },
         &|a, b| a + b,
     )
 }
@@ -40,6 +81,25 @@ fff: out
 ggg: out
 hhh: ccc fff iii
 iii: out";
-    let result = parse_and_solve(input.lines().map(|s| s.to_owned()));
+    let result = parse_and_solve_1(input.lines().map(|s| s.to_owned()));
     assert_eq!(result, 5);
+}
+
+#[test]
+fn day11_2() {
+    let input = "svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out";
+    let result = parse_and_solve_2(input.lines().map(|s| s.to_owned()));
+    assert_eq!(result, 2);
 }
